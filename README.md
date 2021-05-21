b4696fc3e7152b1786e314d07734d3feb137d041
Mathaeneus-Rex <mathew.scurrght@gmail.com>
2021-05-20 20:27:39 -0400

    resubmit because of repository mis-naming

---
+++ README.md
@@ -0,0 +1,11 @@
+this is my shot at the password generator assignment.
+
+I think i did fairly well however I did have issues with my initial upload so I held off until I had completed all the code.
+
+I tried using checkboxes and slider bars for my prompts as I dislike the asthetic and in your face-ness of alert prompts and I enjoy this interface more.
+
+I also will spend more time fixing the slider bar when I think that the time spent will give more improvement.
+
+![](assets/screenshot.jpg)
+
+Never.Stop.Coding.
---
+++ script.js
@@ -0,0 +1,68 @@
+var characterAmountRange = document.getElementById
+('characterAmountRange')
+var characterAmountNumber = document.getElementById
+('characterAmountNumber')
+var includeUpperCaseElement = document.getElementById
+('includeUpperCase')
+var includeNumbersElement = document.getElementById
+("includeNumbers")
+var includeSymbolsElement = document.getElementById
+("includeSymbols")
+var form = document.getElementById
+('passwordGeneratorForm')
+
+var passwordDisplay = document.getElementById
+('passwordDisplay')
+
+var UPPER_CASE_CHAR_CODES = arrayFromLowToHigh(65,90)
+var LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97,122) 
+var NUMBERS_CHAR_CODES = arrayFromLowToHigh(48,57)
+var SYMBOLS_CHAR_CODES = arrayFromLowToHigh(33,47)
+.concat(arrayFromLowToHigh(58, 64))
+.concat(arrayFromLowToHigh(91, 96))
+.concat(arrayFromLowToHigh(123, 126))
+
+
+
+characterAmountNumber.addEventListener('input', syncCharacterAmount)
+characterAmountRange.addEventListener('input', syncCharacterAmount)
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    var characterAmount = characterAmountNumber.value
+    var includeUpperCase = includeUpperCaseElement.checked
+    var includeNumbers = includeNumbersElement.checked
+    var includeSymbols = includeSymbolsElement.checked
+    var password = generatePassword(characterAmount, includeUpperCase, includeNumbers, includeSymbols)
+    passwordDisplay.innerText = password
+})
+
+function generatePassword(characterAmount,includeNumbers,includeSymbols,includeUpperCase){
+   let charCodes = LOWERCASE_CHAR_CODES
+   if (includeUpperCase) charCodes = charCodes.concat(UPPER_CASE_CHAR_CODES)
+   if (includeNumbers) charCodes = charCodes.concat(NUMBERS_CHAR_CODES)
+   if (includeSymbols) charCodes = charCodes.concat(SYMBOLS_CHAR_CODES)
+    
+   var passwordCharacters = []
+   for (let i = 0; i < characterAmount; i++ ) {
+    var characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+    passwordCharacters.push(String.fromCharCode(characterCode))
+   }
+   return passwordCharacters.join('')
+}
+function arrayFromLowToHigh (low,high) {
+    var array = []
+    for (let i = low; i <= high; i++ ){
+    array.push(i) 
+}
+return array
+}
+
+function syncCharacterAmount(e) {
+    var value = e.target.value
+    characterAmountNumber.value = value
+    characterAmountRange.value = value   
+}
+
+  
